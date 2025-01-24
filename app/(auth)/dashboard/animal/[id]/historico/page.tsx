02ceea2453682aefd7a599142d4042c9794986e7
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2025-01-23 21:42:03 -0300

    feat: added username who registered weight

--- app/(auth)/dashboard/animal/[id]/historico/page.tsx
+++ app/(auth)/dashboard/animal/[id]/historico/page.tsx
@@ -13,30 +13,40 @@ export default async function WeightHistory({ params }: WeightHistoryProps) {
 	const { id } = await params;
 	const history = await fetchAnimalWeights({
 		id,
-		rel: ['animal.species'],
+		rel: ['animal.species', 'user'],
 	});
 
 	return (
-		<div className="grid grid-cols-4 gap-4">
-			<div className="col-span-3">
+		<div className="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+			<div className="lg:col-span-2 xl:col-span-3">
 				<WeightsChart history={history} id={id} />
 			</div>
 			{history.weights.length > 0 && (
 				<div className="border p-4 flex flex-col gap-2">
 					<div className="grid grid-cols-2 p-2">
-						<span className="font-semibold text-center">Peso</span>
-						<span className="font-semibold text-center">Registrado em</span>
+						<span className="font-semibold text-center text-sm xl:text-base">
+							Peso
+						</span>
+						<span className="font-semibold text-center text-sm xl:text-base">
+							Registrado
+						</span>
 					</div>
 
-					{[...history.weights].reverse().map((weight) => (
+					{history.weights.map((weight) => (
 						<div
 							className="grid grid-cols-2 border p-2 items-center hover:bg-muted-foreground/10 transition-all"
 							key={weight.id}
 						>
 							<span className="font-medium text-center">{weight.weight}kg</span>
-							<span className="text-sm text-muted-foreground text-center">
-								{formatDistanceAfterADay(weight.created_at)}
-							</span>
+							<div className="flex flex-col gap-2">
+								<span className="text-sm text-neutral-700 text-center">
+									{formatDistanceAfterADay(weight.created_at)}
+								</span>
+
+								<span className="font-medium text-center text-xs text-muted-foreground text-ellipsis overflow-hidden text-nowrap">
+									{truncateName(weight.user?.name)}
+								</span>
+							</div>
 						</div>
 					))}
 				</div>
@@ -56,4 +66,29 @@ function formatDistanceAfterADay(date: Date) {
 	}
 
 	return format(date, 'dd MMM yyyy');
+}
+
+function truncateName(
+	name: string = '',
+	options: { truncatePartIn: number; truncateNameIn: number } = {
+		truncatePartIn: 10,
+		truncateNameIn: 20,
+	},
+) {
+	if (!name) {
+		return 'Não identificado';
+	}
+
+	const parts = name.split(' ');
+	const { truncateNameIn, truncatePartIn } = options;
+
+	const allowedParts: string[] = [];
+	parts.forEach((part) => {
+		const totalLength = allowedParts.join(' ').length + part.length;
+		if (part.length < truncatePartIn && totalLength < truncateNameIn) {
+			allowedParts.push(part);
+		}
+	});
+
+	return allowedParts.join(' ');
 }
